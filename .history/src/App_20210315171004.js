13968de2ad85606726511fcd673cf22e1cf259af
GeryZlatev <68344068+GeryZlatev@users.noreply.github.com>
2021-03-15 17:17:48 +0200

    add footer component folder
    change file architecture

---
+++ .history/src/App_20210315171004.js
@@ -0,0 +1,23 @@
+// import logo from './logo.svg';
+// import { Fragment } from 'react';
+import slider from './media/slider.jpg';
+import style from './App.module.css';
+import Navbar from './comoponents/Navbar';
+import Footer from './comoponents/Footer';
+import SearchBar from './comoponents/SearchBar';
+
+function App() {
+  return (
+    <div className={style.app}>
+      <Navbar />
+      <SearchBar />
+      <div className="image-wrapper">
+      <img src={slider}  alt="beauty woman"/>
+      </div>
+      <Footer/>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/comoponents/Footer/Footer.js
@@ -0,0 +1,80 @@
+import React, { Fragment, Component } from 'react';
+import style from './Footer.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import logo from '../../media/beautyportal_logo.png';
+
+
+import {
+faInstagramSquare,
+faLinkedin,
+faYoutubeSquare,
+faFacebookSquare,
+faTwitterSquare
+} from '@fortawesome/free-brands-svg-icons';
+
+library.add(
+faInstagramSquare,
+faLinkedin,
+faYoutubeSquare,
+faFacebookSquare,
+faTwitterSquare
+);
+
+class Footer extends Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+    render() {
+        return (
+            <Fragment>
+                <footer>
+                    <div className={style.contacts}>
+                        <ul className={style.contactsList}>
+                            <li>
+                                <h5>Head Office</h5>
+                                <p>+(359)888 000 000</p>
+                                <p>beauty_portal@gmail.com</p>
+                            </li>
+                            <li>
+                                <h5>Feedback</h5>
+                                <p>+(359)888 000 001</p>
+                                <p>15 Samokov, Sofia</p>
+                            </li>
+                            <li>
+                                <h5>Connect With Us</h5>
+                                <ul className={style.socialMedia}>
+                                    <li className = {style.listItem}><a href="#"><FontAwesomeIcon icon={faFacebookSquare} /></a></li>
+                                    <li className = {style.listItem}><a href="#"><FontAwesomeIcon icon={faTwitterSquare} /></a></li>
+                                    <li className = {style.listItem}><a href= "#"><FontAwesomeIcon icon={ faInstagramSquare}/></a></li>
+                                    <li className = {style.listItem}><a href="#"><FontAwesomeIcon icon={ faLinkedin}/></a></li>
+                                    <li className = {style.listItem}><a href="#"><FontAwesomeIcon icon={ faYoutubeSquare}/></a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                        
+                    </div>
+                    <div className={style.logo}>
+                        <img src={logo}/>
+                    </div>
+                    <div className={style.portals}>
+                        <ul className={style.portalList}>
+                            <li>
+                                <h5><a href="">My Patient Portal</a></h5>
+                            </li>
+                            <li>
+                                <h5><a href="">Doctor Login</a></h5>
+                            </li>
+                            <li>
+                                <h5><a href="">Staff information</a></h5>
+                            </li>
+                        </ul>
+                        <p className = {style.allRights}> &copy; 2021 All Rights Reserved | <a href="#"> Privacy Policy</a> </p>
+                    </div>
+                </footer>
+            </Fragment>
+        )
+    }
+}
+
+export default Footer;
