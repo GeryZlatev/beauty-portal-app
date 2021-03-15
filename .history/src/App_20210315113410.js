8339c9fd4f98d0024fc9810fc1f8d1520d19ed61
GeryZlatev <68344068+GeryZlatev@users.noreply.github.com>
2021-03-15 11:45:14 +0200

    add css to navbar buttons

---
+++ .history/src/App_20210315113410.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+import { Fragment } from 'react';
+import imageOne from './media/imageOne.jpg';
+import './App.css';
+import Navbar from './comoponents/Navbar';
+
+function App() {
+  return (
+    <Fragment>
+    <Navbar />
+      <div className="image-wrapper">
+      <img src={imageOne}  alt="beauty woman"/>
+      </div>
+    </Fragment>
+
+  );
+}
+
+export default App;
