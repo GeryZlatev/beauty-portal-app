071137b0dd345ba2c8a4a680a78925c0a237d9f7
GeryZlatev <68344068+GeryZlatev@users.noreply.github.com>
2021-03-24 14:25:02 +0200

    reorganize login component
    add error messages

---
+++ .history/src/components/Auth/Login/Login_20210324141532.js
@@ -0,0 +1,67 @@
+import React, {useState} from 'react';
+import Input from '../Input';
+import style from './Login.module.css';
+import Error from '../../ErrorMessage';
+import { withRouter } from 'react-router-dom';
+import { auth } from '../../../services/firebase';
+
+const Login = (props) => {
+
+    const [email, setEmail] = useState('');
+    const [user, setUser] = usestate('');
+    const [errorLogin, setErrorLogin] = useState('');
+
+    const clearErrors = () => {
+        setErrorLogin('');
+    }
+
+    const handleLogin = (e) => {
+    e.preventDefault();
+    clearErrors();
+    auth
+        .signInWithEmailAndPassword(email, password)
+        .then((res) => {
+            setUser(res.user.uid);
+            props.history.push('/')
+        })
+        .catch(err => {
+            setErrorLogin(err.message);
+    })
+}
+    return (
+        <div className={style["login-wrapper"]}>
+            <form>
+                <h3>Login now...</h3>
+                <Input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                >
+                    Email
+                </Input>
+                {errorLogin ? <Error>{ errorLogin }</Error> : null}
+                <Input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    defaultValue={e.target.password}
+                    // onChange={(e) => setPassword(e.target.value)}
+                >
+                    Password
+                </Input>
+                {passwordErr ? <Error>{ passwordErr }</Error> : null}
+                <input type="submit" value="Login" onClick={ handleLogin}/>
+            </form>
+            <p>You do not have an account..!?</p>
+            <button className={style["register-button"]}
+                onClick={()=> props.history.push('/patients/register')}
+            >
+                Register
+            </button>
+        </div>
+    )
+}
+
+export default withRouter(Login);
---
+++ .history/src/components/Auth/Register/Register_20210324141622.js
@@ -0,0 +1,102 @@
+import React, { useState } from 'react';
+import { withRouter} from 'react-router-dom';
+import {auth} from '../../../services/firebase';
+import style from './Register.module.css';
+import Input from '../Input';
+import Error from '../../ErrorMessage';
+
+
+function Register(props) {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [emailErr, setEmailErr] = useState('');
+    const [repeatPassword, setRepeatPassword] = useState('');
+    const [passwordErr, setPasswordErr] = useState('');
+    const [user, setUser] = useState('');
+
+    const clearErrors = () => {
+        setEmailErr('');
+        setPasswordErr('');
+    }
+
+    const handleSignup = (e) => {
+        e.preventDefault()
+        clearErrors()
+        if (password !== repeatPassword) {
+            setPasswordErr('Passwords missmatch!');
+            setPassword("");
+            setRepeatPassword("");
+            return null;
+        }
+
+    auth
+        .createUserWithEmailAndPassword(email, password)
+        .then(()=> {
+            props.history.push('/patients/login')
+        })
+    .catch(err => {
+            setEmailErr(err.message);
+    })
+    }
+
+        return (
+            <div className={style["register-wrapper"]}>
+                <h3><span className={style["first-part"]}>Join us!</span><span className={ style["second-part"]}>Register now</span></h3>
+                <form onSubmit={handleSignup}>
+                    <Input
+                        type="email"
+                        name="email"
+                        value={email}
+                        placeholder="example@email.com"
+                        onChange={(e) => {
+                            setEmail(e.target.value)
+                        }}
+                    >
+                        Email
+                    </Input>
+                    {emailErr ? <Error>{ emailErr}</Error> : null}
+                    <Input
+                        type="password"
+                        name="password"
+                        placeholder="******"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    >
+                        Password
+                    </Input>
+                    <Input
+                        type="password"
+                        name="repeatPassword"
+                        value={repeatPassword}
+                        placeholder="confirm password"
+                        onChange={e => {
+                            setRepeatPassword(e.target.value)
+                            if (password !== repeatPassword) {
+                                setPasswordErr('Passwords mismatch!');
+                            } else {
+                                setPasswordErr("");
+                            }
+                        }}
+                    >
+                        Repeat Password
+                    </Input>
+                    {passwordErr ? <Error>{passwordErr}</Error>: null}
+                    
+                    <input
+                        type="submit"
+                        value="Sign Up"
+                    />
+                </form>
+                    <p>You already have an account..!?</p>
+                <button
+                    className={style["loggin-button"]}
+                    onClick={() => props.history.push('/patients/login') }
+                >
+                    Log in
+                </button>
+            </div>
+        )
+    }
+
+export default withRouter(Register)
